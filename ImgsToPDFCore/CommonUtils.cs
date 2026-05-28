3540bb711ffb410a19dd2dfdf2241d51b1fd5311
Sinryou <li_yunghsing@outlook.com>
2026-05-28 23:31:32 +0800

    简化using语句为using声明

--- ImgsToPDFCore/CommonUtils.cs
+++ ImgsToPDFCore/CommonUtils.cs
@@ -29,12 +29,11 @@ namespace ImgsToPDFCore
         /// <param name="fromFilePath">待解压文件全路径</param>
         /// <param name="outFileDirectory">解压文件后目录</param>
         public static bool Decompress(string fromFilePath, string outFileDirectory) {
-            using (var archive = ArchiveFactory.OpenArchive(fromFilePath)) {
-                if (archive.Entries.Where(p => !p.IsDirectory).First().IsEncrypted) {
-                    return false;
-                }
-                return ExtraArchive(archive, outFileDirectory);
+            using var archive = ArchiveFactory.OpenArchive(fromFilePath);
+            if (archive.Entries.Where(p => !p.IsDirectory).First().IsEncrypted) {
+                return false;
             }
+            return ExtraArchive(archive, outFileDirectory);
         }
         /// <summary>
         /// 解压缩加密的包(不支持rar，支持zip)
